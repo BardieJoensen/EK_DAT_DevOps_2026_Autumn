acc9abea0208daf05d4c9898a9eea3790a664c5c
keamanden <than0002@stud.kea.dk>
2026-09-18 12:19:33 +0200

    Update backend and frontend URLs for devops-gruppe-2

--- repositories.py
+++ repositories.py
@@ -74,8 +74,8 @@ GROUP_REPOS = [
     {
     "name": "devops-gruppe-2",
     "gitLinks": ["https://github.com/devops-gruppe-2/search-engine"],
-    "backend": "",
-    "frontend": "",
+    "backend": "http://20.250.11.23:8080/api/",
+    "frontend": "http://20.250.11.23:8080/register",
     "monitoring": "",
     "stack": [],
     "documentation": [],
