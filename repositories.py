7027f5bdaaf1f8588b57693f098d5a24143d4a77
keamanden <than0002@stud.kea.dk>
2026-09-18 14:30:05 +0200

    Update frontend URL in repositories.py

--- repositories.py
+++ repositories.py
@@ -75,7 +75,7 @@ GROUP_REPOS = [
     "name": "devops-gruppe-2",
     "gitLinks": ["https://github.com/devops-gruppe-2/search-engine"],
     "backend": "http://20.250.11.23:8080/api/",
-    "frontend": "http://20.250.11.23:8080/register",
+    "frontend": "http://20.250.11.23:8080/",
     "monitoring": "",
     "stack": [],
     "documentation": [],
